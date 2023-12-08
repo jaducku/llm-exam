f70790ad765ed48e75c1b3a6543ce6d7a79e623c
김성지 <jaducku@gmail.com>
2023-12-08 00:13:41 +0000

    delete api key

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,11 @@
 import os
 import openai
 
-os.environ["OPENAI_API_KEY"] = "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" # 환경변수에 OPENAI_API_KEY를 설정합니다.
+os.environ["OPENAI_API_KEY"] = "" # 환경변수에 OPENAI_API_KEY를 설정합니다.
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 system_prompt = """
-넌 유능한 통역사야. 처음 상대방이 인사를 하면 동일한 언어로 인사를 건내고 어느나라 말로 통역할지 물어본 후 답변을 받으면 그 말로 통역을 해줘.
+넌 유능한 통역사야. 처음 상대방이 인사를 하면 동일한 언어로 인사를 건내고 어느나라 말로 통역할지 물어본 후 답변을 받으면 그 통역을 해줘.
 """
 
 def openai_llm(input_text, chat_history):
