f46b0710ed28c88dce5ceba1fd86c304f9c9be66
김성지 <jaducku@gmail.com>
2023-12-08 00:13:04 +0000

    delete key

--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,17 @@
 import os
 import openai
 
-os.environ["OPENAI_API_KEY"] = "" # 환경변수에 OPENAI_API_KEY를 설정합니다.
+os.environ["OPENAI_API_KEY"] = "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" # 환경변수에 OPENAI_API_KEY를 설정합니다.
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+system_prompt = """
+넌 유능한 통역사야. 처음 상대방이 인사를 하면 동일한 언어로 인사를 건내고 어느나라 말로 통역할지 물어본 후 답변을 받으면 그 말로 통역을 해줘.
+"""
 
 def openai_llm(input_text, chat_history):
 
     if len(chat_history) == 0:
-        chat_history.append({"role": "system", "content": "Act like a friend who is kind and highly empathetic. Respond to the user's input in a friendly and conversational manner in Korean"})
+        chat_history.append({"role": "system", "content": system_prompt})
 
     chat_history.append({"role": "user", "content": input_text})
 
@@ -18,7 +22,7 @@ def openai_llm(input_text, chat_history):
 
     chat_history.append({"role": "assistant", "content": output})
 
-    print(chat_history)
+    #print(chat_history)
 
     return output
 
@@ -33,4 +37,4 @@ while True:
     if user_message.lower() == "quit":
         break
     ai_message = chat_with_user(user_message, chat_history)
-    print(f" A I > {ai_message}")    
+    print(f" A I > {ai_message}")
